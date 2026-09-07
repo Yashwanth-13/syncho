23c6605e40123d3ca42c078fbf52b4898a823b3c
Yashwanth Chilaka <yashwanth.chilaka@gmail.com>
2026-09-07 22:55:47 +0530

    Refactoring

---
+++ src/config_manager/mod.rs
@@ -0,0 +1,86 @@
+
+use dialoguer::{Input, Select};
+use ping;
+use std::{env, fs, io::{self, BufWriter}};
+use std::{self, path::Path, fs::File, net::Ipv4Addr};
+use serde::{self, Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+    player: String,
+    token: String,
+    peer_ip: String
+}
+
+fn is_reachable(peer_ip: &String) -> bool {
+    let ip = peer_ip.parse::<Ipv4Addr>();
+
+    match ip {
+        Ok(val) => {
+            match ping::new(std::net::IpAddr::V4(val))
+            .send() {
+                Ok(_) => true,
+                Err(_) => false
+            }
+        },
+        Err(e) => false
+    }
+}
+
+fn get_ip() -> String {
+    loop {
+        let peer_ip = Input::new()
+        .with_prompt("Friend's IP address")
+        .interact_text()
+        .unwrap();
+
+        if is_reachable(&peer_ip) {
+            return peer_ip
+        } else {
+            println!("Enter a valid IP address\n")
+        }
+    }
+}
+
+fn create_config(path: &Path) -> Config {
+    println!("\nNo previous config found. Creating new config\n");
+    
+    let valid_players = vec!["Cider", "Spotify"];
+    let peer_ip;
+    let api_key: String;
+
+    let selection = Select::new()
+        .with_prompt("What is your music player?")
+        .items(&valid_players)
+        .interact()
+        .unwrap();
+
+    api_key = Input::new()
+        .with_prompt(format!("Enter your API key for {}" , valid_players[selection]))
+        .interact_text()
+        .unwrap();
+
+    peer_ip = get_ip();
+
+    let file = File::create_new(path).unwrap();
+
+    let cnfg = Config {player: valid_players[selection].to_string(), token: api_key, peer_ip: peer_ip};
+
+    serde_json::to_writer(BufWriter::new(file), &cnfg);
+
+    cnfg
+
+}
+
+pub fn get_config() -> Config {
+    let file_path = format!("{}/.syncho", env::home_dir().unwrap().to_str().unwrap());
+    let path = Path::new(&file_path);
+    
+    if path.exists() {
+        let config_data = fs::read_to_string(&file_path).unwrap();
+        let config: Config = serde_json::from_str(&config_data).unwrap();
+        config
+    } else {
+        create_config(&path)
+    }
+}
